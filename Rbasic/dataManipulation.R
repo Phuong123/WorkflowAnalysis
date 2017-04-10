e038203ab727a500d8048f04a4480932f2174481
phuong <phthph@gmail.com>
2017-04-10 13:11:27 +0200

    Add knit and pandoc commands to dataManipulation

--- Rbasic/dataManipulation.R
+++ Rbasic/dataManipulation.R
@@ -5,3 +5,8 @@
 
 ## For Lap
 setwd("~/Dropbox/WorkflowAnalysis/Rbasic")
+
+# library(knitr)
+# library(markdown)
+# knit("README.Rmd")  # produces the md file
+# pandoc("README.md", format = "html")  # converts md file into docx
