39c4869ffff721cab447e2f35fea7d4b371e4e13
phuong <phthph@gmail.com>
2017-05-26 16:48:39 +0200

    Build a simple graph

--- Rbasic/reviewGraph.R
+++ Rbasic/reviewGraph.R
@@ -27,3 +27,22 @@ ggplot(data,aes(x=Bval, y=Value, fill=Aval)) + geom_bar(stat = "identity", posit
 # change to line graph
 ggplot(data,aes(x=Aval, y=Value, colour=Bval, group=Bval)) + geom_line()
 
+
+############# Build a simple graph ############# 
+dat <- data.frame(xval=1:4, yval=c(3,5,6,9), group=c("A","B","A","B"))
+dat 
+
+# Do not know that we want: line, bar, or others
+ggplot(dat, aes(x=xval, y=yval))
+
+# Add point 
+ggplot(dat, aes(x=xval, y=yval)) + geom_point()
+
+# Add group color
+p <- ggplot(dat, aes(x=xval, y=yval))
+p + geom_point(aes(colour=group))
+
+#
+p + geom_point(colour="blue")
+p + geom_point() + scale_x_continuous(limits=c(0,8))
+p + geom_point(aes(colour=group)) + scale_colour_manual(values=c("orange","forestgreen"))
