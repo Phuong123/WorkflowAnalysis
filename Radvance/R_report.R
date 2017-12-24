c7f249e827fefe3023ed1929ad88f4081141698c
phuong <phthph@gmail.com>
2017-12-24 14:38:00 +0100

    Update R_report.R

--- Radvance/R_report.R
+++ Radvance/R_report.R
@@ -6,7 +6,9 @@
 library(rmarkdown)
 
 # Create example.html webpage with the report content in example.Rmd
-render(example.Rmd, "html_document") 
+# render(example.Rmd, "html_document") 
+setwd("~/rworkspace/WorkflowAnalysis/Radvance/")
+render("women.Rmd", "html_document") 
 
 # There are R markdown, ODT, MS Word and LaTex templates (maybe more)
 
