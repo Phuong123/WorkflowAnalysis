1313423f5c6360b377ef7461fd9b9b1ac58d7cc4
phuong <phthph@gmail.com>
2017-12-24 23:25:00 +0100

    R reports using Markdown or LaTex

--- Radvance/R_report.R
+++ Radvance/R_report.R
@@ -1,29 +1,27 @@
 # Creating a dynamic report
+# There are R markdown, ODT, MS Word and LaTex templates (maybe more)
 
-# Put the report or a template of the report in file.Rmd
-# Call functions from rmarkdown library to generate the report
-
+## Create the report in file.Rmd. Example women.Rmd
 library(rmarkdown)
-
-# Create example.html webpage with the report content in example.Rmd
-
 setwd("~/rworkspace/WorkflowAnalysis/Radvance/")
 
 # Use html format
 # render("women.Rmd", "html_document") 
 
 # Use pdf format - change corresponding text in Rmd
-# render("women.Rmd", "pdf_document") 
+render("women.Rmd", "pdf_document") 
 
 # Use docx format - change corresponding text in Rmd
-render("women.Rmd", "word_document") 
+# render("women.Rmd", "word_document") 
 
-# There are R markdown, ODT, MS Word and LaTex templates (maybe more)
+
 
 ## Create Reports with R and LaTex
 library(knitr)
 knit("RandLaTex.Rnw")
 knit2pdf("RandLaTex.Rnw")
+
+
 
 
 ## Creating dynamic reports with R and OpenOffice
