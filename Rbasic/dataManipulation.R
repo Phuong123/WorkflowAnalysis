e36bcbdcfb7052b97d3a48392c72ba4e77927e91
phuong <phthph@gmail.com>
2017-12-24 14:29:43 +0100

    Create Reports with R

--- Rbasic/dataManipulation.R
+++ Rbasic/dataManipulation.R
@@ -4,9 +4,13 @@
 # setwd("~/rworkspace/WorkflowAnalysis/Rbasic")
 
 ## For Lap
-setwd("~/Dropbox/WorkflowAnalysis/Rbasic")
+# setwd("~/Dropbox/WorkflowAnalysis/Rbasic")
 
 # library(knitr)
 # library(markdown)
-# knit("README.Rmd")  # produces the md file
+# knit("README.Rmd")                    # produces the md file
 # pandoc("README.md", format = "html")  # converts md file into docx
+
+#setwd("~/rworkspace/WorkflowAnalysis/Rbasic")
+
+setwd("~/rworkspace/WorkflowAnalysis/Radvance")
