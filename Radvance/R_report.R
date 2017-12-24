e0ba22acd64196fdc9b8e2ecb539cb98b82f361d
phuong <phthph@gmail.com>
2017-12-24 14:46:15 +0100

    Change to pdf format using corresponding parameters

--- Radvance/R_report.R
+++ Radvance/R_report.R
@@ -6,9 +6,17 @@
 library(rmarkdown)
 
 # Create example.html webpage with the report content in example.Rmd
-# render(example.Rmd, "html_document") 
+
 setwd("~/rworkspace/WorkflowAnalysis/Radvance/")
+
+# Use html format
 render("women.Rmd", "html_document") 
+
+# Use pdf format - change corresponding text in Rmd
+render("women.Rmd", "pdf_document") 
+
+# Use docx format - change corresponding text in Rmd
+render("women.Rmd", "word_document") 
 
 # There are R markdown, ODT, MS Word and LaTex templates (maybe more)
 
