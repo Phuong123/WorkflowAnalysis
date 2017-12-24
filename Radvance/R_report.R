0347b92758275b63e4a0cc0eb5146f103ce39697
phuong <phthph@gmail.com>
2017-12-24 14:33:04 +0100

    Remove html file

---
+++ Radvance/R_report.R
@@ -0,0 +1,12 @@
+# Creating a dynamic report
+
+# Put the report or a template of the report in file.Rmd
+# Call functions from rmarkdown library to generate the report
+
+library(rmarkdown)
+
+# Create example.html webpage with the report content in example.Rmd
+render(example.Rmd, "html_document") 
+
+# There are R markdown, ODT, MS Word and LaTex templates (maybe more)
+
